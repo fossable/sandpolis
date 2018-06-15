56b2d2168fb8129ff5c7cfeb61db59879309cda1
Tyler Cook <tcc@sandpolis.com>
2018-06-15 12:06:07 -0500

    Copy Cmdlet and Exelet superclasses

---
+++ dep/com.sandpolis.core.net/src/main/java/com/sandpolis/core/net/Exelet.java
@@ -0,0 +1,81 @@
+/******************************************************************************
+ *                                                                            *
+ *                    Copyright 2017 Subterranean Security                    *
+ *                                                                            *
+ *  Licensed under the Apache License, Version 2.0 (the "License");           *
+ *  you may not use this file except in compliance with the License.          *
+ *  You may obtain a copy of the License at                                   *
+ *                                                                            *
+ *      http://www.apache.org/licenses/LICENSE-2.0                            *
+ *                                                                            *
+ *  Unless required by applicable law or agreed to in writing, software       *
+ *  distributed under the License is distributed on an "AS IS" BASIS,         *
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  *
+ *  See the License for the specific language governing permissions and       *
+ *  limitations under the License.                                            *
+ *                                                                            *
+ *****************************************************************************/
+package com.sandpolis.core.net;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import com.sandpolis.core.permission.Perm;
+
+/**
+ * An {@code Exelet} handles incoming messages from a {@link Sock}. All
+ * {@code Exelet}s are non-static.
+ * 
+ * @author cilki
+ * @since 5.0.0
+ */
+public abstract class Exelet {
+
+	/**
+	 * The {@link Sock} this {@link Exelet} is handling.
+	 */
+	protected Sock connector;
+
+	public Exelet(Sock connector) {
+		this.connector = connector;
+	}
+
+	/**
+	 * When applied to an {@link Exelet} method, the method will be executable on
+	 * authenticated connections only.
+	 * 
+	 * @author cilki
+	 * @since 5.0.0
+	 */
+	@Retention(RetentionPolicy.RUNTIME)
+	public static @interface Auth {
+	}
+
+	/**
+	 * When applied to an {@link Exelet} method, the method will be executable on
+	 * unauthenticated connections only.
+	 * 
+	 * @author cilki
+	 * @since 5.0.0
+	 */
+	@Retention(RetentionPolicy.RUNTIME)
+	public static @interface Unauth {
+	}
+
+	/**
+	 * When applied to an {@link Exelet} method, the method will be executable on
+	 * connections which have the necessary permission defined in {@link Perm}.
+	 * 
+	 * @author cilki
+	 * @since 5.0.0
+	 */
+	@Retention(RetentionPolicy.RUNTIME)
+	public static @interface Permission {
+
+		/**
+		 * The permission defined in {@link Perm}.
+		 */
+		short permission();
+	}
+
+}
