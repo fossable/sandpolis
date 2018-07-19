15d8d349174587366f26760e25f7c773f486e2d0
Tyler Cook <tcc@sandpolis.com>
2018-07-19 15:35:43 -0500

    Add blank peer encryption handlers to be implemented later

---
+++ dep/com.sandpolis.core.net/src/main/java/com/sandpolis/core/net/codec/PeerEncryptionDecoder.java
@@ -0,0 +1,40 @@
+/******************************************************************************
+ *                                                                            *
+ *                    Copyright 2018 Subterranean Security                    *
+ *                                                                            *
+ *  Licensed under the Apache License, Version 2.0 (the "License");           *
+ *  you may not use this file except in compliance with the License.          *
+ *  You may obtain a copy of the License at                                   *
+ *                                                                            *
+ *      http://www.apache.org/licenses/LICENSE-2.0                            *
+ *                                                                            *
+ *  Unless required by applicable law or agreed to in writing, software       *
+ *  distributed under the License is distributed on an "AS IS" BASIS,         *
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  *
+ *  See the License for the specific language governing permissions and       *
+ *  limitations under the License.                                            *
+ *                                                                            *
+ *****************************************************************************/
+package com.sandpolis.core.net.codec;
+
+import java.util.List;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.MessageToMessageDecoder;
+
+/**
+ * This handler decrypts incoming messages on a peer connection.
+ * 
+ * @author cilki
+ * @since 5.0.0
+ */
+public class PeerEncryptionDecoder extends MessageToMessageDecoder<ByteBuf> {
+
+	@Override
+	protected void decode(ChannelHandlerContext ctx, ByteBuf msg, List<Object> out) throws Exception {
+		// TODO Auto-generated method stub
+		out.add(msg);
+	}
+
+}
