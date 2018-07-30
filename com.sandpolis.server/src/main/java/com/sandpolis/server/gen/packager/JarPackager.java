3edda1e362ba9f7bfe26f398240c3ff0aa89deeb
Tyler Cook <tcc@sandpolis.com>
2018-07-29 20:51:11 -0500

    Add basic url and jar packagers

---
+++ com.sandpolis.server/src/main/java/com/sandpolis/server/gen/packager/JarPackager.java
@@ -0,0 +1,48 @@
+/******************************************************************************
+ *                                                                            *
+ *                    Copyright 2018 Subterranean Security                    *
+ *                                                                            *
+ *  Licensed under the Apache License, Version 2.0 (the "License");           *
+ *  you may not use this file except in compliance with the License.          *
+ *  You may obtain a copy of the License at                                   *
+ *                                                                            *
+ *      http://www.apache.org/licenses/LICENSE-2.0                            *
+ *                                                                            *
+ *  Unless required by applicable law or agreed to in writing, software       *
+ *  distributed under the License is distributed on an "AS IS" BASIS,         *
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  *
+ *  See the License for the specific language governing permissions and       *
+ *  limitations under the License.                                            *
+ *                                                                            *
+ *****************************************************************************/
+package com.sandpolis.server.gen.packager;
+
+import com.google.common.io.Files;
+import com.sandpolis.core.proto.util.Generator.GenConfig;
+import com.sandpolis.core.util.TempUtil;
+import com.sandpolis.server.gen.Packager;
+
+/**
+ * This {@link Packager} produces a runnable jar file.
+ * 
+ * @author cilki
+ * @since 5.0.0
+ */
+public class JarPackager extends Packager {
+
+	@Override
+	public void process(GenConfig config, Object payload) throws Exception {
+		switch (config.getPayload()) {
+		case MEGA:
+			// TODO runtime image
+
+			// TODO create directory for generator output
+			Files.write((byte[]) payload, TempUtil.getFile());
+			break;
+		default:
+			throw new IncompatiblePayloadException();
+		}
+
+	}
+
+}
---
+++ com.sandpolis.server/src/main/java/com/sandpolis/server/gen/packager/UrlPackager.java
@@ -0,0 +1,60 @@
+/******************************************************************************
+ *                                                                            *
+ *                    Copyright 2018 Subterranean Security                    *
+ *                                                                            *
+ *  Licensed under the Apache License, Version 2.0 (the "License");           *
+ *  you may not use this file except in compliance with the License.          *
+ *  You may obtain a copy of the License at                                   *
+ *                                                                            *
+ *      http://www.apache.org/licenses/LICENSE-2.0                            *
+ *                                                                            *
+ *  Unless required by applicable law or agreed to in writing, software       *
+ *  distributed under the License is distributed on an "AS IS" BASIS,         *
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  *
+ *  See the License for the specific language governing permissions and       *
+ *  limitations under the License.                                            *
+ *                                                                            *
+ *****************************************************************************/
+package com.sandpolis.server.gen.packager;
+
+import com.google.common.io.BaseEncoding;
+import com.google.common.io.Files;
+import com.sandpolis.core.proto.util.Generator.GenConfig;
+import com.sandpolis.core.proto.util.Generator.MegaConfig;
+import com.sandpolis.core.proto.util.Generator.MicroConfig;
+import com.sandpolis.core.util.TempUtil;
+import com.sandpolis.server.gen.Packager;
+
+/**
+ * This {@link Packager} produces a URL with an embedded configuration.
+ * 
+ * @author cilki
+ * @since 5.0.0
+ */
+public class UrlPackager extends Packager {
+
+	@Override
+	public void process(GenConfig config, Object payload) throws Exception {
+		String url = "https://sandpolis.com/config?c=";
+
+		switch (config.getPayload()) {
+		case CONFIG:
+			switch (config.getPayloadConfigCase()) {
+			case MEGA:
+				url += BaseEncoding.base64Url().encode(((MegaConfig) payload).toByteArray());
+				break;
+			case MICRO:
+				url += BaseEncoding.base64Url().encode(((MicroConfig) payload).toByteArray());
+				break;
+			default:
+				throw new RuntimeException();
+			}
+			break;
+		default:
+			throw new IncompatiblePayloadException();
+		}
+
+		// TODO create directory for generator output
+		Files.write(url.getBytes(), TempUtil.getFile());
+	}
+}
