d292c59a3e1a61cb5b32bbbe9fb3192eeff17106
Tyler Cook <tcc@sandpolis.com>
2018-08-10 15:37:38 -0500

    Add Hibernate factory

---
+++ dep/com.sandpolis.core.storage.hibernate/src/main/java/com/sandpolis/core/storage/hibernate/HibernateConnection.java
@@ -0,0 +1,61 @@
+/******************************************************************************
+ *                                                                            *
+ *                    Copyright 2018 Subterranean Security                    *
+ *                                                                            *
+ *  Licensed under the Apache License, Version 2.0 (the "License");           *
+ *  you may not use this file except in compliance with the License.          *
+ *  You may obtain a copy of the License at                                   *
+ *                                                                            *
+ *      http://www.apache.org/licenses/LICENSE-2.0                            *
+ *                                                                            *
+ *  Unless required by applicable law or agreed to in writing, software       *
+ *  distributed under the License is distributed on an "AS IS" BASIS,         *
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  *
+ *  See the License for the specific language governing permissions and       *
+ *  limitations under the License.                                            *
+ *                                                                            *
+ *****************************************************************************/
+package com.sandpolis.core.storage.hibernate;
+
+import javax.persistence.EntityManagerFactory;
+
+import com.sandpolis.core.instance.storage.DatabaseConnection;
+import com.sandpolis.core.instance.storage.StoreProvider;
+
+/**
+ * Represents the connection to a Hibernate database.
+ * 
+ * @author cilki
+ * @since 5.0.0
+ */
+public class HibernateConnection extends DatabaseConnection {
+
+	private EntityManagerFactory emf;
+
+	public HibernateConnection(EntityManagerFactory emf) {
+		if (emf == null)
+			throw new IllegalArgumentException();
+
+		this.emf = emf;
+	}
+
+	@Override
+	public boolean isOpen() {
+		return emf.isOpen();
+	}
+
+	@Override
+	public <E> StoreProvider<E> provider(Class<E> cls) {
+		return new HibernateStoreProvider<>(cls, emf);
+	}
+
+	@Override
+	public void close() throws Exception {
+		try {
+			emf.close();
+		} finally {
+			emf = null;
+		}
+	}
+
+}
---
+++ dep/com.sandpolis.core.storage.hibernate/src/main/java/com/sandpolis/core/storage/hibernate/HibernateDatabaseFactory.java
@@ -0,0 +1,100 @@
+/******************************************************************************
+ *                                                                            *
+ *                    Copyright 2018 Subterranean Security                    *
+ *                                                                            *
+ *  Licensed under the Apache License, Version 2.0 (the "License");           *
+ *  you may not use this file except in compliance with the License.          *
+ *  You may obtain a copy of the License at                                   *
+ *                                                                            *
+ *      http://www.apache.org/licenses/LICENSE-2.0                            *
+ *                                                                            *
+ *  Unless required by applicable law or agreed to in writing, software       *
+ *  distributed under the License is distributed on an "AS IS" BASIS,         *
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  *
+ *  See the License for the specific language governing permissions and       *
+ *  limitations under the License.                                            *
+ *                                                                            *
+ *****************************************************************************/
+package com.sandpolis.core.storage.hibernate;
+
+import org.hibernate.cfg.Configuration;
+
+import com.sandpolis.core.instance.storage.Database;
+
+/**
+ * A factory for producing initialized Hibernate databases.
+ * 
+ * @author cilki
+ * @since 5.0.0
+ */
+public final class HibernateDatabaseFactory {
+	private HibernateDatabaseFactory() {
+	}
+
+	/**
+	 * Initialize a new SQLite database with Hibernate.
+	 * 
+	 * @param persist A list of classes that will be persisted or {@code null} for
+	 *                none
+	 * @param db      The database to be initialized
+	 * @return The initialized database
+	 */
+	public static Database sqlite(Class<?>[] persist, Database db) {
+		if (db == null)
+			throw new IllegalArgumentException();
+		if (persist == null)
+			persist = new Class[0];
+
+		Configuration conf = new Configuration()
+				// Set the SQLite database driver
+				.setProperty("connection.driver_class", "org.sqlite.JDBC")
+
+				// Set the SQLite dialect
+				.setProperty("hibernate.dialect", "org.hibernate.dialect.SQLiteDialect")
+
+				// Set the credentials
+				.setProperty("hibernate.connection.username", db.getUsername())
+				.setProperty("hibernate.connection.password", db.getPassword())
+
+				// Set the database URL
+				.setProperty("hibernate.connection.url", db.getUrl())
+
+				// Set additional options
+				.setProperty("hibernate.connection.shutdown", "true").setProperty("hibernate.hbm2ddl.auto", "create")
+				.setProperty("hibernate.current_session_context_class", "thread");
+		for (Class<?> cls : persist)
+			conf.addAnnotatedClass(cls);
+
+		return db.init(new HibernateConnection(conf.buildSessionFactory()));
+	}
+
+	/**
+	 * Initialize a new MySQL database with Hibernate.
+	 * 
+	 * @param persist A list of classes that will be persisted or {@code null} for
+	 *                none
+	 * @param db      The database to be initialized
+	 * @return The initialized database
+	 */
+	public static Database mysql(Class<?>[] persist, Database db) {
+		if (db == null)
+			throw new IllegalArgumentException();
+		if (persist == null)
+			persist = new Class[0];
+
+		Configuration conf = new Configuration()
+				// Set the MySQL database driver
+				.setProperty("connection.driver_class", "com.mysql.jdbc.Driver")
+
+				// Set the MySQL dialect
+				.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect")
+
+				// Set the credentials
+				.setProperty("hibernate.connection.username", db.getUsername())
+				.setProperty("hibernate.connection.password", db.getPassword());
+		for (Class<?> cls : persist)
+			conf.addAnnotatedClass(cls);
+
+		return db.init(new HibernateConnection(conf.buildSessionFactory()));
+	}
+}
