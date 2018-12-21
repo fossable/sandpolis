daffe4c604b7d21aef82a7c4e365ab6970afd0eb
Tyler Cook <tcc@sandpolis.com>
2018-12-20 18:32:23 -0600

    Refactor Environment so that it's more flexible

--- dep/com.sandpolis.core.instance/src/main/java/com/sandpolis/core/instance/Environment.java
+++ dep/com.sandpolis.core.instance/src/main/java/com/sandpolis/core/instance/Environment.java
@@ -23,7 +23,10 @@ import java.lang.management.ManagementFactory;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * This singleton contains information about the runtime environment. Only
@@ -37,8 +40,6 @@ import java.util.Date;
  * @since 4.0.0
  */
 public final class Environment {
-	private Environment() {
-	}
 
 	/**
 	 * The initialization timestamp of the process.
@@ -46,8 +47,7 @@ public final class Environment {
 	public static final Date JVM_TIMESTAMP = new Date(ManagementFactory.getRuntimeMXBean().getStartTime());
 
 	/**
-	 * The location of the main instance jar. May be {@code null} if test mode is
-	 * enabled.
+	 * The location of the main instance jar.
 	 */
 	public static final Path JAR = discoverJar();
 
@@ -56,86 +56,98 @@ public final class Environment {
 	 */
 	public static final Path BASE = discoverBase();
 
-	/**
-	 * The location of the database directory.
-	 */
-	public static final Path DB = discover("db");
+	public enum EnvPath {
+
+		/**
+		 * The location of the database directory.
+		 */
+		DB,
+
+		/**
+		 * The location of the temporary directory.
+		 */
+		TMP,
+
+		/**
+		 * The location of the log directory.
+		 */
+		LOG,
+
+		/**
+		 * The location of the library directory for Java dependencies.
+		 */
+		JLIB,
+
+		/**
+		 * The location of the library directory for native dependencies.
+		 */
+		NLIB,
+
+		/**
+		 * The location of the payload archive.
+		 */
+		GEN;
+	}
+
+	private static Map<EnvPath, Path> envpaths;
 
 	/**
-	 * The location of the temporary directory.
-	 */
-	public static final Path TMP = discover("tmp");
-
-	/**
-	 * The location of the log directory.
-	 */
-	public static final Path LOG = discover("log");
-
-	/**
-	 * The location of the library directory for Java dependencies.
-	 */
-	public static final Path JLIB = discover("jlib");
-
-	/**
-	 * The location of the library directory for native dependencies.
-	 */
-	public static final Path NLIB = discover("nlib");
-
-	/**
-	 * The location of the payload archive.
-	 */
-	public static final Path GEN_OUTPUT = discover("payload");
-
-	/**
-	 * Check that the runtime environment is properly setup.
+	 * Get an environment path.
 	 * 
-	 * @throws RuntimeException If the environment is misconfigured
+	 * @param path The path type
+	 * @return The corresponding {@link Path}
 	 */
-	public static void check() {
+	public static Path get(EnvPath path) {
+		return envpaths.get(path);
+	}
+
+	/**
+	 * Load the filesystem environment.
+	 * 
+	 * @param paths
+	 * @return Whether the environment is set up
+	 */
+	public static boolean load(EnvPath... paths) {
+		if (envpaths != null)
+			throw new IllegalStateException();
+
 		// Run environment checks
 		if (JAR != null && (!Files.exists(JAR) || Files.isDirectory(JAR)))
 			throw new RuntimeException("Failed to locate instance jar.");
-
 		if (BASE == null || !Files.exists(BASE) || !Files.isDirectory(BASE))
 			throw new RuntimeException("Failed to locate base directory.");
 
+		envpaths = Arrays.stream(paths).collect(Collectors.toUnmodifiableMap(p -> p, p -> discover(p)));
+
+		for (Path p : envpaths.values())
+			if (p == null)
+				throw new RuntimeException();
+		for (Path p : envpaths.values())
+			if (!Files.exists(p))
+				return false;
+		return true;
+	}
+
+	/**
+	 * Set up the filesystem environment. This method is idempotent.
+	 */
+	public static void setup() {
 		try {
-			if (!Files.exists(DB))
-				Files.createDirectories(DB);
-			if (!Files.exists(TMP))
-				Files.createDirectories(TMP);
-			if (!Files.exists(LOG))
-				Files.createDirectories(LOG);
-			if (!Files.exists(JLIB))
-				Files.createDirectories(JLIB);
-			if (!Files.exists(NLIB))
-				Files.createDirectories(NLIB);
-			if (!Files.exists(GEN_OUTPUT))
-				Files.createDirectories(GEN_OUTPUT);
+			for (Path p : envpaths.values()) {
+				if (!Files.exists(p))
+					Files.createDirectories(p);
+				if (!Files.isDirectory(p))
+					throw new RuntimeException("Expected directory: " + p);
+			}
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
 	}
 
 	/**
-	 * Print the environment for debugging.
-	 */
-	public static void print() {
-		System.out.println("\t=== ENVIRONMENT ===");
-		System.out.println("JVM TIMESTAMP: " + JVM_TIMESTAMP.toString());
-		System.out.println("BASE: " + BASE);
-		System.out.println("DB: " + DB);
-		System.out.println("TMP: " + TMP);
-		System.out.println("LOG: " + LOG);
-		System.out.println("JLIB: " + JLIB);
-		System.out.println("GEN_OUTPUT: " + GEN_OUTPUT);
-	}
-
-	/**
 	 * Locate the instance jar.
 	 * 
-	 * @return A {@link File} representing the main jar file or {@code null} if an
-	 *         error occurred
+	 * @return A {@link Path} representing the main jar file
 	 */
 	private static Path discoverJar() {
 		try {
@@ -170,14 +182,28 @@ public final class Environment {
 	 * symbolic links.
 	 * 
 	 * @param sub The desired subdirectory
-	 * @return A {@link File} representing the subdirectory or {@code null} if an
+	 * @return A {@link Path} representing the subdirectory or {@code null} if an
 	 *         error occurred
 	 */
-	private static Path discover(String sub) {
+	private static Path discover(EnvPath sub) {
 		try {
-			return BASE.resolve(sub).toFile().getCanonicalFile().toPath();
+			return BASE.resolve(sub.name()).toFile().getCanonicalFile().toPath();
 		} catch (IOException e) {
-			throw new RuntimeException(e);
+			return null;
 		}
+	}
+
+	/**
+	 * Print the environment for debugging.
+	 */
+	public static void print() {
+		System.out.println("\t=== ENVIRONMENT ===");
+		System.out.println("JVM TIMESTAMP: " + JVM_TIMESTAMP.toString());
+		System.out.println("BASE: " + BASE);
+		for (EnvPath p : envpaths.keySet())
+			System.out.println(p + ": " + envpaths.get(p));
+	}
+
+	private Environment() {
 	}
 }
