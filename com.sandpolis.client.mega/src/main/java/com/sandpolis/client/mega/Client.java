613f9d1aa4f482138858bdb9f187d6112437fe22
Tyler Cook <tcc@sandpolis.com>
2019-01-03 21:27:00 -0600

    Add client entry

---
+++ com.sandpolis.client.mega/src/main/java/com/sandpolis/client/mega/Client.java
@@ -0,0 +1,260 @@
+/******************************************************************************
+ *                                                                            *
+ *                    Copyright 2015 Subterranean Security                    *
+ *                                                                            *
+ *  Licensed under the Apache License, Version 2.0 (the "License");           *
+ *  you may not use this file except in compliance with the License.          *
+ *  You may obtain a copy of the License at                                   *
+ *                                                                            *
+ *      http://www.apache.org/licenses/LICENSE-2.0                            *
+ *                                                                            *
+ *  Unless required by applicable law or agreed to in writing, software       *
+ *  distributed under the License is distributed on an "AS IS" BASIS,         *
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  *
+ *  See the License for the specific language governing permissions and       *
+ *  limitations under the License.                                            *
+ *                                                                            *
+ *****************************************************************************/
+package com.sandpolis.client.mega;
+
+import static com.sandpolis.core.instance.Environment.EnvPath.DB;
+import static com.sandpolis.core.instance.Environment.EnvPath.JLIB;
+import static com.sandpolis.core.instance.Environment.EnvPath.LOG;
+import static com.sandpolis.core.instance.Environment.EnvPath.NLIB;
+import static com.sandpolis.core.instance.Environment.EnvPath.TMP;
+import static com.sandpolis.core.net.store.network.NetworkStore.Events.SRV_ESTABLISHED;
+import static com.sandpolis.core.net.store.network.NetworkStore.Events.SRV_LOST;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Date;
+import java.util.concurrent.Executors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.sandpolis.client.mega.cmd.AuthCmd;
+import com.sandpolis.core.instance.BasicTasks;
+import com.sandpolis.core.instance.Config;
+import com.sandpolis.core.instance.Core;
+import com.sandpolis.core.instance.Environment;
+import com.sandpolis.core.instance.MainDispatch;
+import com.sandpolis.core.instance.MainDispatch.InitializationTask;
+import com.sandpolis.core.instance.MainDispatch.TaskOutcome;
+import com.sandpolis.core.instance.Signaler;
+import com.sandpolis.core.instance.storage.MemoryListStoreProvider;
+import com.sandpolis.core.instance.store.artifact.ArtifactStore;
+import com.sandpolis.core.instance.store.plugin.Plugin;
+import com.sandpolis.core.instance.store.plugin.PluginStore;
+import com.sandpolis.core.instance.store.thread.ThreadStore;
+import com.sandpolis.core.ipc.IPCTasks;
+import com.sandpolis.core.net.store.connection.ConnectionStore;
+import com.sandpolis.core.proto.util.Auth.KeyContainer;
+import com.sandpolis.core.proto.util.Generator.MegaConfig;
+import com.sandpolis.core.proto.util.Platform.Instance;
+import com.sandpolis.core.util.AsciiUtil;
+import com.sandpolis.core.util.CryptoUtil.SAND5.ReciprocalKeyPair;
+
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.util.concurrent.UnorderedThreadPoolEventExecutor;
+
+/**
+ * The entry point for Client instances. This class is responsible for
+ * initializing the new instance.
+ *
+ * @author cilki
+ * @since 1.0.0
+ */
+public final class Client {
+
+	private static final Logger log = LoggerFactory.getLogger(Client.class);
+
+	/**
+	 * The configuration included in the instance jar.
+	 */
+	public static final MegaConfig SO_CONFIG;
+
+	static {
+		try {
+			SO_CONFIG = MegaConfig.parseFrom(MainDispatch.getMain().getResourceAsStream("/soi/client.bin"));
+		} catch (IOException e) {
+			throw new RuntimeException("Failed to read SO_CONFIG!", e);
+		}
+	}
+
+	public static void main(String[] args) {
+		log.info("Launching {} ({})", AsciiUtil.toRainbow("Sandpolis Client"), Core.SO_BUILD.getVersion());
+		log.debug("Built on {} with {} (Build: {})", new Date(Core.SO_BUILD.getTime()), Core.SO_BUILD.getPlatform(),
+				Core.SO_BUILD.getNumber());
+
+		MainDispatch.register(BasicTasks::loadConfiguration);
+		MainDispatch.register(IPCTasks::checkLocks);
+		MainDispatch.register(Client::install);
+		MainDispatch.register(Client::loadEnvironment);
+		MainDispatch.register(Client::loadStores);
+		MainDispatch.register(Client::loadPlugins);
+		MainDispatch.register(Client::beginConnectionRoutine);
+	}
+
+	/**
+	 * Install the client if necessary.
+	 *
+	 * @return The task's outcome
+	 */
+	@InitializationTask(name = "Install client", fatal = true)
+	private static TaskOutcome install() {
+		TaskOutcome task = TaskOutcome.begin(new Object() {
+		}.getClass().getEnclosingMethod());
+
+		if (Environment.JAR == null)
+			return task.skipped();
+
+		// Installation mode
+		Path base = Paths.get(SO_CONFIG.getExecution().getInstallPathOrDefault(0, "."));// TODO 0
+		Path lib = base.resolve("jlib");
+
+		try {
+			Files.createDirectories(base);
+			Files.createDirectories(lib);
+		} catch (IOException e) {
+			return task.failure(e);
+		}
+
+		try {
+			Files.copy(Client.class.getResourceAsStream("/main/main.jar"), base.resolve("client.jar"));
+		} catch (Exception e) {
+			return task.failure(e);
+		}
+
+		ArtifactStore.getDependencies(Instance.CLIENT).forEach(artifact -> {
+			String name = ArtifactStore.getArtifactFilename(artifact.getCoordinates());
+
+			try {
+				Files.copy(Client.class.getResourceAsStream("/lib/" + name), lib.resolve(name));
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		});
+
+		System.exit(0);
+		return task.failure();
+	}
+
+	/**
+	 * Load the runtime environment.
+	 *
+	 * @return The task's outcome
+	 */
+	@InitializationTask(name = "Load runtime environment", fatal = true)
+	private static TaskOutcome loadEnvironment() {
+		TaskOutcome task = TaskOutcome.begin(new Object() {
+		}.getClass().getEnclosingMethod());
+
+		if (!Environment.load(DB, TMP, LOG, JLIB, NLIB)) {
+			try {
+				Environment.setup();
+			} catch (RuntimeException e) {
+				return task.failure(e);
+			}
+		}
+
+		return task.success();
+	}
+
+	/**
+	 * Load static stores.
+	 * 
+	 * @return The task's outcome
+	 */
+	@InitializationTask(name = "Load static stores", fatal = true)
+	public static TaskOutcome loadStores() {
+		TaskOutcome task = TaskOutcome.begin(new Object() {
+		}.getClass().getEnclosingMethod());
+
+		// Load ThreadStore
+		ThreadStore.register(Executors.newSingleThreadExecutor(r -> {
+			var s = new Thread(r, "SIGNALER");
+			s.setDaemon(true);
+			return s;
+		}), "signaler");
+		ThreadStore.register(new NioEventLoopGroup(4), "net.exelet");
+		ThreadStore.register(new NioEventLoopGroup(2), "net.connection.outgoing");
+		ThreadStore.register(new UnorderedThreadPoolEventExecutor(2), "net.message.incoming");
+		Signaler.init(ThreadStore.get("signaler"));
+
+		// Load PluginStore
+		PluginStore.init(new MemoryListStoreProvider<Plugin>(Plugin.class));
+
+		return task.success();
+	}
+
+	/**
+	 * Load plugins.
+	 *
+	 * @return The task's outcome
+	 */
+	@InitializationTask(name = "Load client plugins")
+	private static TaskOutcome loadPlugins() {
+		TaskOutcome task = TaskOutcome.begin(new Object() {
+		}.getClass().getEnclosingMethod());
+
+		if (Config.getBoolean("no_plugins"))
+			return task.skipped();
+
+		try {
+			PluginStore.installCore();
+
+			PluginStore.loadPlugins();
+		} catch (Exception e) {
+			return task.failure(e);
+		}
+
+		return task.success();
+	}
+
+	/**
+	 * Begin the connection routine.
+	 *
+	 * @return The task's outcome
+	 */
+	@InitializationTask(name = "Begin the connection routine", fatal = true)
+	private static TaskOutcome beginConnectionRoutine() {
+		TaskOutcome task = TaskOutcome.begin(new Object() {
+		}.getClass().getEnclosingMethod());
+
+		Signaler.register(SRV_ESTABLISHED, () -> {
+			var auth = SO_CONFIG.getAuthentication();
+			try {
+				switch (auth.getAuthOneofCase()) {
+				case KEY:
+					KeyContainer mech = auth.getKey();
+					ReciprocalKeyPair key = new ReciprocalKeyPair(mech.getClient().getVerifier().toByteArray(),
+							mech.getClient().getSigner().toByteArray());
+					AuthCmd.key(auth.getGroupName(), mech.getId(), key);
+					break;
+				case PASSWORD:
+					AuthCmd.password(auth.getPassword().getPassword());
+					break;
+				default:
+					AuthCmd.none();
+					break;
+				}
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		});
+
+		Signaler.register(SRV_LOST, () -> {
+			ConnectionStore.connect(SO_CONFIG.getNetwork().getLoopConfig(), new Class[] {});
+		});
+		Signaler.fire(SRV_LOST);
+
+		return task.success();
+	}
+
+	private Client() {
+	}
+}
---
+++ com.sandpolis.client.mega/src/main/java/com/sandpolis/client/mega/cmd/AuthCmd.java
@@ -0,0 +1,106 @@
+/******************************************************************************
+ *                                                                            *
+ *                    Copyright 2017 Subterranean Security                    *
+ *                                                                            *
+ *  Licensed under the Apache License, Version 2.0 (the "License");           *
+ *  you may not use this file except in compliance with the License.          *
+ *  You may obtain a copy of the License at                                   *
+ *                                                                            *
+ *      http://www.apache.org/licenses/LICENSE-2.0                            *
+ *                                                                            *
+ *  Unless required by applicable law or agreed to in writing, software       *
+ *  distributed under the License is distributed on an "AS IS" BASIS,         *
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  *
+ *  See the License for the specific language governing permissions and       *
+ *  limitations under the License.                                            *
+ *                                                                            *
+ *****************************************************************************/
+package com.sandpolis.client.mega.cmd;
+
+import static com.sandpolis.core.util.ProtoUtil.begin;
+import static com.sandpolis.core.util.ProtoUtil.complete;
+import static com.sandpolis.core.util.ProtoUtil.getOutcome;
+import static com.sandpolis.core.util.ProtoUtil.rq;
+import static com.sandpolis.core.util.ProtoUtil.rs;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import com.google.protobuf.ByteString;
+import com.sandpolis.core.net.Cmdlet;
+import com.sandpolis.core.net.Timeouts;
+import com.sandpolis.core.net.exception.MessageFlowException;
+import com.sandpolis.core.net.store.network.NetworkStore;
+import com.sandpolis.core.proto.net.MCAuth.IM_Nonce;
+import com.sandpolis.core.proto.net.MCAuth.RQ_KeyAuth;
+import com.sandpolis.core.proto.net.MCAuth.RQ_NoAuth;
+import com.sandpolis.core.proto.net.MCAuth.RQ_PasswordAuth;
+import com.sandpolis.core.proto.net.MSG.Message;
+import com.sandpolis.core.proto.util.Result.Outcome;
+import com.sandpolis.core.util.CryptoUtil;
+import com.sandpolis.core.util.CryptoUtil.SAND5.ReciprocalKeyPair;
+
+/**
+ * Contains authentication commands for client instances.
+ * 
+ * @author cilki
+ * @since 5.0.0
+ */
+public final class AuthCmd extends Cmdlet {
+
+	/**
+	 * Attempt to authenticate with nothing.
+	 * 
+	 * @return The outcome of the action
+	 */
+	public static Outcome none() throws InterruptedException, ExecutionException, TimeoutException {
+		return getOutcome(NetworkStore
+				.route(rq().setRqNoAuth(RQ_NoAuth.newBuilder()), Timeouts.DEFAULT, TimeUnit.MILLISECONDS).get());
+	}
+
+	/**
+	 * Attempt to authenticate with a password.
+	 * 
+	 * @return The outcome of the action
+	 */
+	public static Outcome password(String password) throws InterruptedException, ExecutionException, TimeoutException {
+		return getOutcome(NetworkStore.route(rq().setRqPasswordAuth(RQ_PasswordAuth.newBuilder().setPassword(password)),
+				Timeouts.DEFAULT, TimeUnit.MILLISECONDS).get());
+	}
+
+	/**
+	 * Attempt to authenticate with a SAND5 key.
+	 * 
+	 * @return The outcome of the action
+	 */
+	public static Outcome key(String groupId, long mechId, ReciprocalKeyPair key)
+			throws InterruptedException, ExecutionException, TimeoutException {
+		Outcome.Builder outcome = begin();
+
+		byte[] nonceA = CryptoUtil.SAND5.getNonce();
+		Message rs = NetworkStore.route(rq().setRqKeyAuth(
+				RQ_KeyAuth.newBuilder().setGroupId(groupId).setMechId(mechId).setNonce(ByteString.copyFrom(nonceA))),
+				Timeouts.DEFAULT, TimeUnit.MILLISECONDS).get();
+		if (rs.getRsOutcome() != null)
+			return rs.getRsOutcome();
+		if (rs.getImNonce() == null)
+			throw new MessageFlowException(RQ_KeyAuth.class, rs);
+
+		byte[] signed = CryptoUtil.SAND5.sign(key, rs.getImNonce().getNonce().toByteArray());
+		rs = NetworkStore.route(rq().setImNonce(IM_Nonce.newBuilder().setNonce(ByteString.copyFrom(signed))),
+				Timeouts.DEFAULT, TimeUnit.MILLISECONDS).get();
+		if (rs.getRsOutcome() != null)
+			return rs.getRsOutcome();
+		if (rs.getImNonce() == null)
+			throw new MessageFlowException(IM_Nonce.class, rs);
+
+		boolean verifyServer = CryptoUtil.SAND5.check(key, nonceA, rs.getImNonce().getNonce().toByteArray());
+		NetworkStore.route(rs(rs).setRsOutcome(Outcome.newBuilder().setResult(verifyServer)));
+
+		return complete(outcome.setResult(verifyServer));
+	}
+
+	private AuthCmd() {
+	}
+}
