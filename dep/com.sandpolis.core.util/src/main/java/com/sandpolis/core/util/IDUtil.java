fossable/sandpolis

commit 42f3f76d1413a618c0d80ae50ee4c9d2ddaa86df

Restrict CVID domain to positive integers

dep/com.sandpolis.core.util/src/main/java/com/sandpolis/core/util/IDUtil.java
@@ -81,7 +81,7 @@ public final class IDUtil {
 			if (instance == Instance.UNRECOGNIZED)
 				throw new IllegalArgumentException();
 
-			return (RandUtil.nextInt() << IID_SPACE) | instance.getNumber();
+			return Math.abs((RandUtil.nextInt() << IID_SPACE) | instance.getNumber());
 		}
 
 	}

commit 54ee7ccc6a4cede7232ccfc5a566db9455cd5d2f

Fix embarrassing bug introduced in 42f3f76.
Math.abs should not be used because it interferes with the IID segment.

dep/com.sandpolis.core.util/src/main/java/com/sandpolis/core/util/IDUtil.java
@@ -19,6 +19,8 @@ package com.sandpolis.core.util;
 
 import static com.sandpolis.core.util.CryptoUtil.SHA256;
 
+import java.util.Objects;
+
 import com.sandpolis.core.proto.util.Platform.Instance;
 import com.sandpolis.core.proto.util.Platform.InstanceFlavor;
 
@@ -50,8 +52,6 @@ public final class IDUtil {
 	 * @since 5.0.0
 	 */
 	public static final class CVID {
-		private CVID() {
-		}
 
 		/**
 		 * The number of bits used to encode the instance ID (IID).
@@ -70,20 +70,26 @@ public final class IDUtil {
 		}
 
 		/**
-		 * Generate a new CVID.
+		 * Generate a new random CVID.<br>
+		 * <br>
+		 * Note: there's a small chance that this method will produce an invalid ID of 0
+		 * for Charcoal instances. Since Charcoal is for debugging only, this is not
+		 * remedied by introducing a validity-checking loop. Charcoal instances should
+		 * manually check the output of this method and regenerate if equal to 0.
 		 * 
 		 * @param instance The new CVID's instance type
 		 * @return A new CVID
 		 */
 		public static int cvid(Instance instance) {
-			if (instance == null)
-				throw new IllegalArgumentException();
+			Objects.requireNonNull(instance);
 			if (instance == Instance.UNRECOGNIZED)
 				throw new IllegalArgumentException();
 
-			return Math.abs((RandUtil.nextInt() << IID_SPACE) | instance.getNumber());
+			return ((RandUtil.nextInt() << IID_SPACE) | instance.getNumber()) & 0x7FFFFFFF;
 		}
 
+		private CVID() {
+		}
 	}
 
 	/**