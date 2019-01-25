d6143e040af3be9028a1c6e84d6b480624c4dcfb
Tyler Cook <tcc@sandpolis.com>
2019-01-24 23:50:07 -0600

    Add micro generator stub

---
+++ com.sandpolis.server/src/main/java/com/sandpolis/server/gen/generator/MicroGen.java
@@ -0,0 +1,42 @@
+/******************************************************************************
+ *                                                                            *
+ *                    Copyright 2018 Subterranean Security                    *
+ *                                                                            *
+ *  Licensed under the Apache License, Version 2.0 (the "License");           *
+ *  you may not use this file except in compliance with the License.          *
+ *  You may obtain a copy of the License at                                   *
+ *                                                                            *
+ *      http://www.apache.org/licenses/LICENSE-2.0                            *
+ *                                                                            *
+ *  Unless required by applicable law or agreed to in writing, software       *
+ *  distributed under the License is distributed on an "AS IS" BASIS,         *
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  *
+ *  See the License for the specific language governing permissions and       *
+ *  limitations under the License.                                            *
+ *                                                                            *
+ *****************************************************************************/
+package com.sandpolis.server.gen.generator;
+
+import com.sandpolis.core.proto.util.Generator.GenConfig;
+import com.sandpolis.server.gen.FileGenerator;
+
+/**
+ * This generator builds a MICRO client.
+ * 
+ * @author cilki
+ * @since 5.0.0
+ */
+public class MicroGen extends FileGenerator {
+
+	public MicroGen(GenConfig config) {
+		super(config);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected Object run() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
