fossable/sandpolis

commit bdde9fce0f535f344c55880cf7b625de5d18e3a4

Move event dispatch responsibility from ConnectionStore to Sock

dep/com.sandpolis.core.net/src/main/java/com/sandpolis/core/net/loop/ConnectionLoop.java
@@ -29,7 +29,6 @@ import org.slf4j.LoggerFactory;
 import com.sandpolis.core.net.Sock;
 import com.sandpolis.core.net.future.SockFuture;
 import com.sandpolis.core.net.init.ChannelConstant;
-import com.sandpolis.core.net.store.connection.ConnectionStore;
 import com.sandpolis.core.proto.util.Generator.LoopConfig;
 import com.sandpolis.core.proto.util.Generator.NetworkTarget;
 
@@ -124,12 +123,6 @@ public class ConnectionLoop extends Thread {
 					} catch (TimeoutException | ExecutionException e) {
 						log.trace("Connection attempt timed out after {} ms", timeout);
 						result = null;
-					}
-
-					if (result != null) {
-						// TODO move somewhere else
-						ConnectionStore.add(result);
-						return;
 					}
 
 					time = System.currentTimeMillis() - time;

com.sandpolis.server/src/main/java/com/sandpolis/server/exe/LoginExe.java
@@ -28,7 +28,6 @@ import org.slf4j.LoggerFactory;
 import com.sandpolis.core.net.Exelet;
 import com.sandpolis.core.net.Sock;
 import com.sandpolis.core.net.Sock.ConnectionState;
-import com.sandpolis.core.net.store.connection.ConnectionStore;
 import com.sandpolis.core.proto.net.MSG.Message;
 import com.sandpolis.core.proto.util.Result.Outcome;
 import com.sandpolis.core.util.ValidationUtil;
@@ -53,7 +52,7 @@ public class LoginExe extends Exelet {
 		log.debug("Processing logout request from: {}", connector.getRemoteIP());
 
 		connector.send(rs(m).setRsOutcome(Outcome.newBuilder().setResult(true)));
-		ConnectionStore.close(connector);
+		connector.close();
 	}
 
 	@Unauth

dep/com.sandpolis.core.instance/src/main/java/com/sandpolis/core/instance/Signaler.java
@@ -136,11 +136,11 @@ public final class Signaler<E> {
 	 */
 	public static <E> void fire(Object type, E e) {
 		Objects.requireNonNull(type);
+		log.debug("Firing event: {}", type);
 
-		if (listeners.containsKey(type)) {
-			log.debug("Firing event: {}", type);
+		if (listeners.containsKey(type))
 			pool.execute(() -> listeners.get(type).removeIf(f -> !f.apply(e)));
-		}
+
 	}
 
 	/**

dep/com.sandpolis.core.net/src/main/java/com/sandpolis/core/net/Sock.java
@@ -34,8 +34,10 @@ import javax.net.ssl.SSLPeerUnverifiedException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.sandpolis.core.instance.Signaler;
 import com.sandpolis.core.net.future.MessageFuture;
 import com.sandpolis.core.net.init.ChannelConstant;
+import com.sandpolis.core.net.store.connection.ConnectionStore.Events;
 import com.sandpolis.core.proto.net.MCPing.RQ_Ping;
 import com.sandpolis.core.proto.net.MSG.Message;
 import com.sandpolis.core.proto.util.Platform.Instance;
@@ -103,11 +105,19 @@ public class Sock {
 	public void changeState(ConnectionState state) {
 		var attribute = channel.attr(ChannelConstant.CONNECTION_STATE);
 		if (attribute.get() == state)
-			throw new IllegalArgumentException();
+			throw new IllegalArgumentException("Attempted to change state to itself: " + state);
 
 		log.debug("[CVID {}] Connection state changed: {}->{}", getRemoteCvid(), attribute.get(), state);
 		synchronized (attribute) {
 			attribute.set(state);
+		}
+
+		switch (state) {
+		case NOT_CONNECTED:
+			Signaler.fire(Events.SOCK_LOST, this);
+			break;
+		default:
+			break;
 		}
 	}
 
@@ -135,8 +145,10 @@ public class Sock {
 	public Sock(Channel channel) {
 		this.channel = Objects.requireNonNull(channel);
 
-		if (getState() == null)
-			changeState(NOT_CONNECTED);
+		if (getState() == CONNECTED) {
+			log.debug("[CVID {}] Connection state changed: {}->{}", getRemoteCvid(), NOT_CONNECTED, CONNECTED);
+			Signaler.fire(Events.SOCK_ESTABLISHED, this);
+		}
 	}
 
 	/**
@@ -145,7 +157,6 @@ public class Sock {
 	public void close() {
 		requireState(CONNECTED, AUTHENTICATED);
 
-		changeState(NOT_CONNECTED);
 		channel.close();
 		channel.eventLoop().shutdownGracefully();
 	}
@@ -350,7 +361,8 @@ public class Sock {
 	 * @return A {@link MessageFuture}
 	 */
 	public MessageFuture read(int id, int timeout, TimeUnit unit) {
-		return channel.attr(ChannelConstant.HANDLER_EXECUTE).get().putResponseFuture(id, new MessageFuture(timeout, unit));
+		return channel.attr(ChannelConstant.HANDLER_EXECUTE).get().putResponseFuture(id,
+				new MessageFuture(timeout, unit));
 	}
 
 	/**

dep/com.sandpolis.core.net/src/main/java/com/sandpolis/core/net/store/connection/ConnectionStore.java
@@ -43,7 +43,6 @@ import com.sandpolis.core.net.store.network.NetworkStore;
 import com.sandpolis.core.proto.util.Generator.LoopConfig;
 
 import io.netty.bootstrap.Bootstrap;
-import io.netty.util.concurrent.DefaultPromise;
 
 /**
  * A static store for managing direct connections and connection attempt
@@ -84,47 +83,14 @@ public final class ConnectionStore extends Store {
 		SOCK_ESTABLISHED;
 	}
 
-	/**
-	 * Add a connection to the store.
-	 * 
-	 * @param con The connection
-	 */
-	public static void add(Sock con) {
-		Objects.requireNonNull(con);
-		if (connections.containsKey(con.getRemoteCvid()))
-			throw new IllegalArgumentException();
-		log.debug("New sock registered with: {}", con.getRemoteCvid());
+	static {
+		Signaler.register(SOCK_LOST, (Sock sock) -> {
+			connections.remove(sock.getRemoteCvid());
+		});
 
-		connections.put(con.getRemoteCvid(), con);
-		Signaler.fire(SOCK_ESTABLISHED, con);
-	}
-
-	/**
-	 * Close and remove a connection from the store.
-	 * 
-	 * @param cvid The CVID of the connection to remove
-	 * @return The closed connection or {@code null} if the connection was not found
-	 */
-	public static Sock close(int cvid) {
-		Sock removal = connections.remove(cvid);
-		if (removal != null) {
-			removal.close();
-			log.debug("Sock with: {} closed", cvid);
-
-			Signaler.fire(SOCK_LOST, removal);
-		}
-		return removal;
-	}
-
-	/**
-	 * Close and remove a connection from the store.
-	 * 
-	 * @param con The connection to remove
-	 */
-	public static void close(Sock con) {
-		Objects.requireNonNull(con);
-
-		close(con.getRemoteCvid());
+		Signaler.register(SOCK_ESTABLISHED, (Sock sock) -> {
+			connections.put(sock.getRemoteCvid(), sock);
+		});
 	}
 
 	/**
@@ -170,12 +136,7 @@ public final class ConnectionStore extends Store {
 	public static SockFuture connect(Bootstrap bootstrap) {
 		Objects.requireNonNull(bootstrap);
 
-		SockFuture sf = new SockFuture(bootstrap.connect());
-		sf.addListener((DefaultPromise<Sock> future) -> {
-			add(future.get());
-		});
-
-		return sf;
+		return new SockFuture(bootstrap.connect());
 	}
 
 	/**

dep/com.sandpolis.core.net/src/main/java/com/sandpolis/core/net/store/network/NetworkStore.java
@@ -87,7 +87,7 @@ public final class NetworkStore {
 
 	static {
 		Signaler.register(SOCK_LOST, (Sock sock) -> {
-			network.removeEdge(network.edgeConnectingOrNull(Core.cvid(), sock.getRemoteCvid()));
+			network.edgeConnecting(Core.cvid(), sock.getRemoteCvid()).ifPresent(network::removeEdge);
 
 			// See if that was the last connection to a server
 			if (sock.getRemoteInstance() == Instance.SERVER) {

commit 105469e598b67ee6ef62d29b7340bd49733fcf8a

Fix loop error introduced in bdde9fce0f535f344c55880cf7b625de5d18e3a4

dep/com.sandpolis.core.net/src/main/java/com/sandpolis/core/net/loop/ConnectionLoop.java
@@ -125,6 +125,9 @@ public class ConnectionLoop extends Thread {
 						result = null;
 					}
 
+					if (result != null)
+						return;
+
 					time = System.currentTimeMillis() - time;
 					if (time < timeout)
 						Thread.sleep(timeout - time);