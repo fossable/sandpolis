2d1e316bb8025cca84f940bfe753e1bd25854a91
Tyler Cook <tcc@sandpolis.com>
2019-12-24 17:21:18 -0600

    [skip ci] Update CliInstaller

--- com.sandpolis.installer/src/main/java/com/sandpolis/installer/CliInstaller.java
+++ com.sandpolis.installer/src/main/java/com/sandpolis/installer/CliInstaller.java
@@ -11,21 +11,12 @@
 //=========================================================S A N D P O L I S==//
 package com.sandpolis.installer;
 
-import com.sandpolis.core.soi.SoiUtil;
-import com.sandpolis.core.util.ArtifactUtil;
-import com.sandpolis.installer.util.InstallUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.InputStream;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.List;
 import java.util.Objects;
-import java.util.Set;
 import java.util.concurrent.Callable;
-
-import static com.sandpolis.core.util.ArtifactUtil.ParsedCoordinate.fromCoordinate;
 
 /**
  * @author cilki
@@ -35,125 +26,37 @@ public class CliInstaller implements Callable<Void> {
 
 	private static final Logger log = LoggerFactory.getLogger(CliInstaller.class);
 
-	/**
-	 * The installation directory.
-	 */
-	private Path destination;
+	private Installer installer;
 
-	/**
-	 * The Sandpolis instance that will be installed.
-	 */
-	private String coordinate;
-
-	/**
-	 * The client configuration.
-	 */
-	protected String config;
-
-	/**
-	 * Whether the installation completed successfully.
-	 */
-	private boolean completed;
-
-	/**
-	 * A list of installation extensions.
-	 */
-	private List<Runnable> extensions;
-
-	protected CliInstaller(Path destination) {
-		this.destination = Objects.requireNonNull(destination);
+	protected CliInstaller(Installer installer) {
+		this.installer = Objects.requireNonNull(installer);
+		this.installer.status = log::debug;
 	}
 
 	public static CliInstaller newServerInstaller(Path destination) {
-		CliInstaller installer = new CliInstaller(destination);
-		installer.coordinate = "com.sandpolis:sandpolis-server-vanilla:";
-		return installer;
+		Installer installer = new Installer(destination, "com.sandpolis:sandpolis-server-vanilla:");
+		return new CliInstaller(installer);
 	}
 
 	public static CliInstaller newViewerJfxInstaller(Path destination) {
-		CliInstaller installer = new CliInstaller(destination);
-		installer.coordinate = "com.sandpolis:sandpolis-viewer-jfx:";
-		return installer;
+		Installer installer = new Installer(destination, "com.sandpolis:sandpolis-viewer-jfx:");
+		return new CliInstaller(installer);
 	}
 
 	public static CliInstaller newViewerCliInstaller(Path destination) {
-		CliInstaller installer = new CliInstaller(destination);
-		installer.coordinate = "com.sandpolis:sandpolis-viewer-cli:";
-		return installer;
+		Installer installer = new Installer(destination, "com.sandpolis:sandpolis-viewer-cli:");
+		return new CliInstaller(installer);
 	}
 
 	public static CliInstaller newClientInstaller(Path destination, String config) {
-		CliInstaller installer = new CliInstaller(destination);
-		installer.coordinate = "com.sandpolis:sandpolis-client-mega:";
+		Installer installer = new Installer(destination, "com.sandpolis:sandpolis-client-mega:");
 		installer.config = config;
-		return installer;
+		return new CliInstaller(installer);
 	}
 
 	@Override
 	public Void call() throws Exception {
-		log.debug("Executing installation for " + coordinate);
-
-		String version = Main.VERSION;
-		if (version == null || version.equalsIgnoreCase("latest")) {
-			// Request latest version number
-			log.info("Downloading metadata");
-			version = ArtifactUtil.getLatestVersion(coordinate);
-		}
-		coordinate += version;
-
-		// Create directories
-		Path lib = destination.resolve("lib");
-		Files.createDirectories(lib);
-
-		// Download executable
-		log.info("Downloading " + coordinate);
-		Path executable = ArtifactUtil.download(lib, coordinate);
-
-		// Calculate dependencies
-		Set<String> dependencies = InstallUtil.computeDependencies(SoiUtil.readMatrix(executable), coordinate);
-
-		for (String dep : dependencies) {
-			var coordinate = fromCoordinate(dep);
-			Path dependency = lib.resolve(coordinate.filename);
-			if (!Files.exists(dependency)) {
-				InputStream in = CliInstaller.class.getResourceAsStream("/" + coordinate.filename);
-				if (in != null) {
-					log.info("Extracting " + dep);
-					try (in) {
-						Files.copy(in, dependency);
-					}
-				} else {
-					log.info("Downloading " + dep);
-					ArtifactUtil.download(lib, dep);
-				}
-			}
-		}
-
-		if (Main.IS_LINUX) {
-			if (coordinate.contains(":sandpolis-viewer-jfx:")) {
-				Path bin = InstallUtil.installLinuxBinaries(executable, coordinate);
-				Path icon = InstallUtil.installIcon(executable, "/image/icon@4x.png",
-						executable.getParent().resolveSibling("Sandpolis.png"));
-				InstallUtil.installLinuxDesktopEntry(bin, icon, coordinate, "Sandpolis Viewer");
-			}
-		}
-
-		else if (Main.IS_WINDOWS) {
-			if (coordinate.contains(":sandpolis-viewer-jfx:")) {
-				Path bin = InstallUtil.installWindowsBinaries(executable, coordinate);
-				Path icon = InstallUtil.installIcon(executable, "/image/icon.ico",
-						executable.getParent().resolveSibling("Sandpolis.ico"));
-				InstallUtil.installWindowsStartMenuEntry(bin, icon, "Sandpolis Viewer");
-				InstallUtil.installWindowsDesktopShortcut(bin, icon, "Sandpolis Viewer");
-			}
-		}
-
-		completed = true;
+		installer.run();
 		return null;
 	}
-
-	public boolean isCompleted() {
-		return completed;
-	}
-
 }
