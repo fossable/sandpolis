1ca3341cc3f5a5e7a570d8d00b0cdc6e31583665
Tyler Cook <tcc@sandpolis.com>
2020-03-25 20:54:14 -0500

    Make long values explicit in TextUtilTest

--- module/com.sandpolis.core.util/src/test/java/com/sandpolis/core/util/TextUtilTest.java
+++ module/com.sandpolis.core.util/src/test/java/com/sandpolis/core/util/TextUtilTest.java
@@ -12,6 +12,8 @@
 package com.sandpolis.core.util;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static com.sandpolis.core.util.TextUtil.formatByteCount;
+import static com.sandpolis.core.util.TextUtil.formatByteCountSI;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -22,37 +24,37 @@ class TextUtilTest {
 	@DisplayName("Format byte counts")
 	void formatByteCount_1() {
 
-		assertEquals("0 B", formatByteCount(0));
-		assertEquals("27 B", formatByteCount(27));
-		assertEquals("999 B", formatByteCount(999));
-		assertEquals("1000 B", formatByteCount(1000));
-		assertEquals("1023 B", formatByteCount(1023));
-		assertEquals("1.0 KiB", formatByteCount(1024));
-		assertEquals("1.7 KiB", formatByteCount(1728));
-		assertEquals("108.0 KiB", formatByteCount(110592));
-		assertEquals("6.8 MiB", formatByteCount(7077888));
-		assertEquals("432.0 MiB", formatByteCount(452984832));
-		assertEquals("27.0 GiB", formatByteCount(28991029248));
-		assertEquals("1.7 TiB", formatByteCount(1855425871872));
-		assertEquals("8.0 EiB", formatByteCount(9223372036854775807));
+		assertEquals("0 B", formatByteCount(0L));
+		assertEquals("27 B", formatByteCount(27L));
+		assertEquals("999 B", formatByteCount(999L));
+		assertEquals("1000 B", formatByteCount(1000L));
+		assertEquals("1023 B", formatByteCount(1023L));
+		assertEquals("1.0 KiB", formatByteCount(1024L));
+		assertEquals("1.7 KiB", formatByteCount(1728L));
+		assertEquals("108.0 KiB", formatByteCount(110592L));
+		assertEquals("6.8 MiB", formatByteCount(7077888L));
+		assertEquals("432.0 MiB", formatByteCount(452984832L));
+		assertEquals("27.0 GiB", formatByteCount(28991029248L));
+		assertEquals("1.7 TiB", formatByteCount(1855425871872L));
+		assertEquals("8.0 EiB", formatByteCount(9223372036854775807L));
 	}
 
 	@Test
 	@DisplayName("Format byte counts in SI system")
 	void formatByteCountSI_1() {
 
-		assertEquals("0 B", formatByteCountSI(0));
-		assertEquals("27 B", formatByteCountSI(27));
-		assertEquals("999 B", formatByteCountSI(999));
-		assertEquals("1.0 kB", formatByteCountSI(1000));
-		assertEquals("1.0 kB", formatByteCountSI(1023));
-		assertEquals("1.0 kB", formatByteCountSI(1024));
-		assertEquals("1.7 kB", formatByteCountSI(1728));
-		assertEquals("110.6 kB", formatByteCountSI(110592));
-		assertEquals("7.1 MB", formatByteCountSI(7077888));
-		assertEquals("453.0 MB", formatByteCountSI(452984832));
-		assertEquals("29.0 GB", formatByteCountSI(28991029248));
-		assertEquals("1.9 TB", formatByteCountSI(1855425871872));
-		assertEquals("9.2 EB", formatByteCountSI(9223372036854775807));
+		assertEquals("0 B", formatByteCountSI(0L));
+		assertEquals("27 B", formatByteCountSI(27L));
+		assertEquals("999 B", formatByteCountSI(999L));
+		assertEquals("1.0 kB", formatByteCountSI(1000L));
+		assertEquals("1.0 kB", formatByteCountSI(1023L));
+		assertEquals("1.0 kB", formatByteCountSI(1024L));
+		assertEquals("1.7 kB", formatByteCountSI(1728L));
+		assertEquals("110.6 kB", formatByteCountSI(110592L));
+		assertEquals("7.1 MB", formatByteCountSI(7077888L));
+		assertEquals("453.0 MB", formatByteCountSI(452984832L));
+		assertEquals("29.0 GB", formatByteCountSI(28991029248L));
+		assertEquals("1.9 TB", formatByteCountSI(1855425871872L));
+		assertEquals("9.2 EB", formatByteCountSI(9223372036854775807L));
 	}
 }
