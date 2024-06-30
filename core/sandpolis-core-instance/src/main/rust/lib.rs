3d6eb9bd578b59f0a1ccc8c873355630a829165e
Tyler Cook <10459406+cilki@users.noreply.github.com>
2024-06-30 14:11:12 -0500

    wip: move sources around

--- core/sandpolis-core-instance/src/main/rust/lib.rs
+++
@@ -1,9 +0,0 @@
-use std::io::Result;
-
-pub mod instance {
-    include!(concat!(env!("OUT_DIR"), "/core.instance.rs"));
-}
-
-fn main() -> Result<()> {
-    Ok(())
-}
--- core/sandpolis-core-protocol/src/main/rust/lib.rs
+++
@@ -1,6 +0,0 @@
-
-pub mod core {
-	pub mod protocol {
-	    include!(concat!(env!("OUT_DIR"), "/core.protocol.rs"));
-	}
-}
