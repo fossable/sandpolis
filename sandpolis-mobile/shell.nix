b1583812e61e6cbfe71b1303a8aba57cfa862a24
Tyler Cook <10459406+cilki@users.noreply.github.com>
2025-01-11 19:21:41 -0600

    Update mobile project from template

--- sandpolis-mobile/shell.nix
+++ sandpolis-mobile/shell.nix
@@ -1,3 +1,24 @@
-{ pkgs ? import <nixpkgs> { config.android_sdk.accept_license = true; } }:
+{ pkgs ? import <nixpkgs> { config.allowUnfree = true; } }:
 
-pkgs.mkShell { buildInputs = with pkgs; [ android-studio-full glibc ]; }
+with pkgs;
+
+let
+  android-nixpkgs = callPackage <android-nixpkgs> { channel = "stable"; };
+
+  android-sdk = android-nixpkgs.sdk (sdkPkgs:
+    with sdkPkgs; [
+      cmdline-tools-latest
+      build-tools-34-0-0
+      platform-tools
+      platforms-android-34
+      emulator
+      cmake-3-22-1
+      ndk-26-1-10909125
+    ]);
+
+in mkShell {
+  buildInputs = [ android-studio android-sdk jdk ];
+
+  GRADLE_OPTS =
+    "-Dorg.gradle.project.android.aapt2FromMavenOverride=${android-sdk}/share/android-sdk/build-tools/34.0.0/aapt2";
+}
